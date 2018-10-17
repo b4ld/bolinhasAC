2705ae124e3fd2719c5b9b34d698f2ce6e40ae88
Carmezim <self.pecar@gmail.com>
2018-10-17 22:08:55 +0100

    Add Draw ,grid ,add assets, add lib, add codeScatch

---
+++ assets/ScratchCode/scratchAC.java
@@ -0,0 +1,27 @@
+
+
+
+//função que testa se a troca de bolas foi feita por menos que uma cas
+//retorna false se tentar mudar por mais que uma casa
+//true se so por uma\
+
+
+
+public static void main(String[]args){
+
+
+        public boolean canSwap(int XprimeiroClick, int YprimeiroClick, int XsegundoClick, int YsegundoClick){
+
+            int moveX = Math.abs(XprimeiroClick-XsegundoClick);
+                int moveY = Math.abs(YprimeiroClick-YsegundoClick);
+
+
+
+                return (moveX == 0 && moveY ==1) || (moveX == 10 && moveY ==0);
+        }
+
+
+
+
+
+        }
